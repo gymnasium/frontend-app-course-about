66385a12682b5905cb4e8b241352237c5ac6eeb2
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-01-31 16:56:31 -0500

    update webpack configs

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -3,12 +3,16 @@ const { createConfig } = require('@edx/frontend-build');
 
 const config = createConfig('webpack-prod');
 
+config.experiments = {
+  topLevelAwait: true
+}
+
 config.resolve = {
-  alias: {
-    '@edx/frontend-component-footer': path.resolve(__dirname, '../frontend-component-footer/src'),
-    '@edx/frontend-component-header': path.resolve(__dirname, '../frontend-component-header/src'),
-    'env.config': path.resolve(process.cwd(), './env.config'),
-  },
+  // alias: {
+  //   '@edx/frontend-component-footer': path.resolve(__dirname, '../frontend-component-footer/src'),
+  //   '@edx/frontend-component-header': path.resolve(__dirname, '../frontend-component-header/src'),
+  //   'env.config': path.resolve(process.cwd(), './env.config'),
+  // },
   fallback: {
     // This causes the system to return an empty object if it can't find an env.config.js file in
     // the application being built.
