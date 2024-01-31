aed85bbed8326dd3a27a5b72a5a8d9a98637e0dc
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-01-31 17:07:59 -0500

    again, webpack confuses and confounds

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -7,24 +7,14 @@ config.experiments = {
   topLevelAwait: true
 }
 
-config.resolve = {
-  // alias: {
-  //   '@edx/frontend-component-footer': path.resolve(__dirname, '../frontend-component-footer/src'),
-  //   '@edx/frontend-component-header': path.resolve(__dirname, '../frontend-component-header/src'),
-  //   'env.config': path.resolve(process.cwd(), './env.config'),
-  // },
-  fallback: {
-    // This causes the system to return an empty object if it can't find an env.config.js file in
-    // the application being built.
-    'env.config': false,
-  },
-  extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.css', '.scss', '.config'],
-  preferRelative: true,
-  symlinks: true,
+config.resolve.alias =  {
+  'gym-frontend-components': path.resolve(__dirname, '../gym-frontend-components'),
 };
 
-config.stats = {
-  children: true,
-}
+config.resolve.modules = [
+  path.resolve(__dirname, './node_modules'),
+  path.resolve(__dirname, './src'),
+  'node_modules',
+];
 
 module.exports = config;
